e8a9529da7c253c69723fba88f022fc6fc1eb22c
Petar Nikolov <penikolov23@gmail.com>
2019-07-22 18:38:09 +0300

    [Solution] Document HMIException

--- include/tree/hmi_exception.hh
+++ include/tree/hmi_exception.hh
@@ -1,3 +1,16 @@
+/*!
+ * \file include/tree/hmi_exception.hh
+ * \brief Definition of HMIException
+ *
+ * HMIExceptions are exceptions that are raised when an error occurs int the
+ * operation of an HMITree instance.
+ *
+ * \see src/tree/hmi_exception.cc
+ *
+ * \author Petar Nikolov
+ * \version v1.0.0
+ */
+
 #ifndef HMI_TREE_OPTIMIZATION__TREE__EXCEPTION_HH
 #define HMI_TREE_OPTIMIZATION__TREE__EXCEPTION_HH
 
@@ -6,18 +19,48 @@
 
 #include "tree/node.hh"
 
+//! \namespace hmi_tree_optimization
+//! \brief Project namespace
 namespace hmi_tree_optimization {
+    //! \namespace tree
+    //! \brief Contains a bare bones simulated HMI tree
     namespace tree {
+        /*!
+         * \class HMIException
+         *
+         * \brief Exception to throw when an error occurs in the HMI tree
+         *
+         * This exception can optionally be tied to a tree's node. It is
+         * then apparent that the specified node was the cause of the exception.
+         *
+         * \see Node
+         * \see HMITree
+         */
         class HMIException: public std::exception {
+
         public:
-            explicit HMIException(Node *);
+
+            /*!
+             * \brief Construct HMIException
+             *
+             * \param erroneous_node The node which was the (supposed) cause of the exception.
+             */
+            explicit HMIException(Node *erroneous_node);
+
+            /*!
+             * \brief Return an appropriate error message
+             *
+             * \return Error message, detailing the exception.
+             */
             virtual const char *what() const noexcept override;
+
         private:
-            Node *erroneous_node_;
-            std::string error_msg_;
+
+            Node *erroneous_node_;  /*!< Pointer to optional erroneous node. */
+            std::string error_msg_;  /*!< Error message to display. */
         };  // class HMIException
     }  // namespace tree
 }  // namespace hmi_tree_optimization
 
-#endif
+#endif  // HMI_TREE_OPTIMIZATION__TREE__EXCEPTION_HH
 
--- src/tree/hmi_exception.cc
+++ src/tree/hmi_exception.cc
@@ -1,11 +1,28 @@
+/*!
+ * \file src/tree/hmi_exception.cc
+ * \brief Implementation of HMIException
+ *
+ * \see include/tree/hmi_exception.hh
+ *
+ * \author Petar Nikolov
+ * \version v1.0.0
+ */
+
 #include "tree/hmi_exception.hh"
 
 #include <sstream>
 
 #include "tree/node.hh"
 
+//! \namespace hmi_tree_optimization
+//! \brief Project namespace
 namespace hmi_tree_optimization {
+    //! \namespace tree
+    //! \brief Contains a bare bones simulated HMI tree
     namespace tree {
+        /*!
+         * \brief Construct HMIException
+         */
         HMIException::HMIException(Node *erroneous_node)
             : erroneous_node_(erroneous_node) {
             std::ostringstream msg;
@@ -16,6 +33,9 @@ namespace hmi_tree_optimization {
             error_msg_ = msg.str();
         }
 
+        /*!
+         * \brief Return an appropriate error message
+         */
         const char *HMIException::what() const noexcept {
             return error_msg_.c_str();
         }
