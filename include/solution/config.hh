c93a98959055935f00ec768e62df9b351daf0df0
Petar Nikolov <penikolov23@gmail.com>
2019-07-26 01:54:21 +0300

    [Solution] Document program config files

--- include/solution/config.hh
+++ include/solution/config.hh
@@ -1,11 +1,25 @@
+/*!
+ * \file include/solution/config.hh
+ * \brief Global configuration variables for program
+ *
+ * \see src/solution/config.cc
+ *
+ * \author Petar Nikolov
+ * \version v1.0.0
+ */
+
 #ifndef HMI_TREE_OPTIMIZATION__TREE__CONFIG_HH
 #define HMI_TREE_OPTIMIZATION__TREE__CONFIG_HH
 
+//! \namespace hmi_tree_optimization
+//! \brief Project namespace
 namespace hmi_tree_optimization {
-    namespace tree {
-        extern bool g_debug;  // display debug messages if true
-    }  // namespace tree
+    //! \namespace solution 
+    //! \brief Solution to the presented optimization problem
+    namespace solution {
+        extern bool g_debug;  /*!< Display debug messages. */
+    }  // namespace solution 
 }  // namespace hmi_tree_optimization
 
-#endif
+#endif  // HMI_TREE_OPTIMIZATION__TREE__CONFIG_HH
 
--- src/solution/config.cc
+++ src/solution/config.cc
@@ -1,8 +1,22 @@
+/*!
+ * \file src/solution/config.cc
+ * \brief Default values of global configuration variables
+ *
+ * \see include/solution/config.hh
+ *
+ * \author Petar Nikolov
+ * \version v1.0.0
+ */
+
 #include "solution/config.hh"
 
+//! \namespace hmi_tree_optimization
+//! \brief Project namespace
 namespace hmi_tree_optimization {
-    namespace tree {
+    //! \namespace solution 
+    //! \brief Solution to the presented optimization problem
+    namespace solution {
         bool g_debug = false;
-    }  // namespace tree
+    }  // namespace solution
 }  // namespace hmi_tree_optimization
 
